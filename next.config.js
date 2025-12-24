f2211262bcf21a89ff974da178eb6ad43d32b348
mohamedhannat@gmail.com <mohamedhannat@gmail.com>
2025-12-24 17:04:05 +0300

    Add Amplify configuration

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_API_ENDPOINT: 'https://dvzdg005z5.execute-api.us-east-1.amazonaws.com/prod'
+  }
+}
+
+module.exports = nextConfig
